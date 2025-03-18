a41cec31d90ea8fedafac1a681a6bcb5513ba03a
ranamohyy <ranaamr22@gmail.com>
2025-03-19 01:26:04 +0200

    add styles

--- lib/core/helpers/styles.dart
+++ lib/core/helpers/styles.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
 TextStyle get kTextStyle12White =>
-    TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.white);
+    const TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Colors.white);
 TextStyle get kTextStyle16White =>
-    TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.white);
+    const TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Colors.white);
 TextStyle get kTextStyleAppBarTitle =>
-    TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold);
+    const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold);
 TextStyle get kTextStyle16BoldBlack =>
-    TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
+    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
 TextStyle get kTextStyle12Grey =>
-    TextStyle(fontSize: 12, color: Colors.grey);
+    const TextStyle(fontSize: 12, color: Colors.grey);
 TextStyle get kTextStyle14Grey =>
-    TextStyle(fontSize: 14, color: Colors.grey);
+    const TextStyle(fontSize: 14, color: Colors.grey);
 TextStyle get kTextStyle14UnderLine =>
     TextStyle(fontSize: 14, fontWeight: FontWeight.bold,
         decoration: TextDecoration.underline,
