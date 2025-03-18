efca15ccd7fdf03d8fbd4cabb1ffa06f604e12ca
ranamohyy <ranaamr22@gmail.com>
2025-03-18 21:09:00 +0200

    custom container in home page

---
+++ lib/features/widgets/custom_container_in_home.dart
@@ -0,0 +1,38 @@
+part of '../home/components/home.dart';
+class ContainerInHome extends StatelessWidget {
+  const ContainerInHome({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.blue[50],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(
+            child: Column(
+              spacing: 10,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Get a symptom assessment and begin your journey toward better health",
+                  style: TextStyle(
+                      fontSize: 12, fontWeight: FontWeight.w400),
+                ),
+                MyButton(title: "Start"),
+              ],
+            ),
+          ),
+          Expanded(child: Image.asset(AppImages.doctors))
+
+        ],
+      ),
+    );
+  }
+}
