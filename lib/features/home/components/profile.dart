3c051b6a239e9789c6f262468daac74644abf525
ranamohyy <ranaamr22@gmail.com>
2025-03-19 01:42:33 +0200

    finish My Account screen

--- lib/features/home/components/profile.dart
+++ lib/features/home/components/profile.dart
@@ -2,6 +2,7 @@ import 'package:cura/core/app_navigator.dart';
 import 'package:cura/features/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../privacy_screen/view.dart';
 import '../../settings/view.dart';
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({super.key, this.color});
@@ -14,21 +15,21 @@ class MyAccountPage extends StatelessWidget {
       body: Column(
 
         children: [
-      _buildProfileOption(Icons.person_outline, "Edit profile"),
-      _buildProfileOption(Icons.settings_outlined, "Settings"),
-      _buildProfileOption(Icons.privacy_tip_outlined, "Privacy"),
-      _buildProfileOption(Icons.logout, "Logout", isLogout: true),
+      _buildProfileOption(Icons.person_outline, "Edit profile",(){}),
+      _buildProfileOption(Icons.settings_outlined, "Settings",()=>AppNavigator.push(SettingsView())),
+      _buildProfileOption(Icons.privacy_tip_outlined, "Privacy",()=>AppNavigator.push(PrivacyPolicyScreen())),
+      _buildProfileOption(Icons.logout, "Logout",(){},isLogout: true),
       ],
     ),
     );
   }
-  Widget _buildProfileOption(IconData? icon, String title,{bool isLogout = false}) {
+  Widget _buildProfileOption(IconData? icon, String title,void Function()? onTap,{bool isLogout=false}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
         children: [
           ListTile(
-            leading: Icon(icon, color: isLogout ? Colors.red : Colors.black),
+            leading: Icon(icon, color:isLogout ? Colors.red : Colors.black,),
             title: Text(
               title,
               style: TextStyle(
@@ -37,10 +38,8 @@ class MyAccountPage extends StatelessWidget {
                 color: isLogout ? Colors.red : Colors.black,
               ),
             ),
-            trailing: const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.black54),
-            onTap: () {
-              AppNavigator.push(const SettingsView());
-            },
+            trailing:!isLogout?const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.black54):SizedBox(),
+            onTap: onTap
           ),
           const SizedBox(height: 12,),
           Divider(height: 1, color: Colors.grey.shade300),
