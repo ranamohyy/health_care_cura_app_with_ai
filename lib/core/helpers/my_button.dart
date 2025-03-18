69e855a8a2506d5ba05167e93a3a15f71a6f0ce9
ranamohyy <ranaamr22@gmail.com>
2025-03-18 21:12:08 +0200

    custom button for all features

---
+++ lib/core/helpers/my_button.dart
@@ -0,0 +1,22 @@
+import 'package:cura/core/helpers/styles.dart';
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key, required this.title, this.heightOfButton, this.style,this.onTap});
+  final String title;
+  final TextStyle ?style;
+  final double? heightOfButton;
+ final void Function()?onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size.fromHeight(heightOfButton??30),
+          backgroundColor: AppColors.primary,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+      onPressed: onTap??() {},
+      child: Text(title, style:style?? kTextStyle12White),
+    );
+  }
+}
