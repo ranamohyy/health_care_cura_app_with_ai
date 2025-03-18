fde2ce2c25ca1132ffca19304ab9c3465a59cd70
ranamohyy <ranaamr22@gmail.com>
2025-03-18 20:46:58 +0200

    add assets

---
+++ lib/core/helpers/app_assets.dart
@@ -0,0 +1,44 @@
+const String svg = 'assets/svg';
+const String icons = 'assets/icons';
+const String images = 'assets/images';
+
+class AppIcons {
+  static String calenderFilled = '$icons/calendar_filled.svg';
+  static String backButton = '$icons/back_button.svg';
+  static String archive = '$icons/archive.svg';
+  static String myAppointments = '$icons/appoinments.svg';
+  static String home = '$icons/home.svg';
+  static String chatFilled = '$icons/chat_bot.svg';
+  static String profile = '$icons/profile.svg';
+  static const String calender = '$icons/calender.svg';
+  static const String menu = '$icons/menu.svg';
+  static const String notifications = '$icons/notifications.svg';
+  static const String message = '$icons/messege.svg';
+  static const String language = '$icons/language.svg';
+  static const String mode = '$icons/mode.svg';
+  static const String notificationsOutlined = '$icons/notifications_outlinned.svg';
+  static const String settings = '$icons/settings.svg';
+  static const String deleteAccount = '$icons/delete_account.svg';
+}
+class AppImages {
+  static String medicines = '$images/medicine_time.png';
+  static String robot = '$images/robot.png';
+  static String secondRecommendImage = '$images/second_reccommend.png';
+  static String thirdRecommendImage = '$images/third_reccommend.png';
+  static String fourthRecommendImage = '$images/fourth_reccommend.png';
+  static String fifthRecommendImage = '$images/fifth_reccommend.png';
+  static String sixRecommendImage = '$images/reccommend6.png';
+  static String sevenRecommendImage = '$images/reccommend7.png';
+  static String eightRecommendImage = '$images/reccommend8.png';
+  static String nineRecommendImage = '$images/reccommend9.png';
+  static String tenRecommendImage = '$images/reccommend10.png';
+  static String care = '$images/care.png';
+  static String checkCare = '$images/check.png';
+  static String cologine = '$images/cologine.png';
+  static String corona = '$images/corona.png';
+  static String doctors = '$images/doctors.png';
+  static String emptyChat = '$images/empty_chat.png';
+  static String hospital = '$images/hospital.png';
+  static String wearMask = '$images/mask.png';
+  static const String washHand = '$images/wash.png';
+}
