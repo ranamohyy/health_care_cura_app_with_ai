a3b5e1d61c76a7d541c1db6243aa574e16447b3c
ranamohyy <ranaamr22@gmail.com>
2025-03-19 17:59:04 +0200

    finish profile screen

--- lib/features/home/components/profile.dart
+++ lib/features/home/components/profile.dart
@@ -2,47 +2,38 @@ import 'package:cura/core/app_navigator.dart';
 import 'package:cura/features/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/helpers/app_assets.dart';
+import '../../change_profile/view.dart';
 import '../../privacy_screen/view.dart';
 import '../../settings/view.dart';
+import '../../widgets/card_profile_screen.dart';
+
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({super.key, this.color});
-  final Color ?color;
+  final Color? color;
   @override
   Widget build(BuildContext context) {
-    return
-    Scaffold(
-      appBar: const MyAppBar(title: 'Profile',showBackButton: false,),
+    return Scaffold(
+      appBar: const MyAppBar(
+        title: 'Profile',
+        showBackButton: false,
+      ),
       body: Column(
-
         children: [
-      _buildProfileOption(Icons.person_outline, "Edit profile",(){}),
-      _buildProfileOption(Icons.settings_outlined, "Settings",()=>AppNavigator.push(SettingsView())),
-      _buildProfileOption(Icons.privacy_tip_outlined, "Privacy",()=>AppNavigator.push(PrivacyPolicyScreen())),
-      _buildProfileOption(Icons.logout, "Logout",(){},isLogout: true),
-      ],
-    ),
-    );
-  }
-  Widget _buildProfileOption(IconData? icon, String title,void Function()? onTap,{bool isLogout=false}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          ListTile(
-            leading: Icon(icon, color:isLogout ? Colors.red : Colors.black,),
-            title: Text(
-              title,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: isLogout ? Colors.red : Colors.black,
-              ),
-            ),
-            trailing:!isLogout?const Icon(Icons.arrow_forward_ios, size: 16, color: Colors.black54):SizedBox(),
-            onTap: onTap
+          Image.asset(
+            AppImages.changeProfile,
+            height: 140,
           ),
-          const SizedBox(height: 12,),
-          Divider(height: 1, color: Colors.grey.shade300),
+          const SizedBox(
+            height: 30,
+          ),
+          buildProfileOption(AppIcons.editProfile, "Edit profile",
+              () => AppNavigator.push(const ChangeProfileView()),true),
+          buildProfileOption(AppIcons.setting, "Settings",
+              () => AppNavigator.push(const SettingsView()),true),
+          buildProfileOption(AppIcons.privacy, "Privacy",
+              () => AppNavigator.push(const PrivacyPolicyScreen()),true),
+          buildProfileOption(AppIcons.logOut, "Logout", () {},false, isLogout: true),
         ],
       ),
     );
