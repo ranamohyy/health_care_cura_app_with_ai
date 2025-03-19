125ad081061741f4aacb27041ae3a2d2dae57df3
ranamohyy <ranaamr22@gmail.com>
2025-03-19 17:15:05 +0200

    fix issue with have color in whole bg navigation bar with change theme

--- lib/features/home/nav_home_view.dart
+++ lib/features/home/nav_home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cura/core/helpers/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../core/helpers/CustomSvg.dart';
@@ -14,41 +15,42 @@ class NavUserView extends StatelessWidget {
         final cubit = context.read<NavCubit>();
         return Scaffold(
             backgroundColor: Colors.black,
-            bottomNavigationBar: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                BottomNavigationBar(
-                    currentIndex: currentIndex,
-                    onTap: (page) {
-                      cubit.changePage(page);
-                    },
-                    items: [
-                      BottomNavigationBarItem(
-                          icon: CustomSvg(
-                              svg: AppIcons.home,
-                              color: NavCubit.changeColor(0, currentIndex)),
-                          label: "Home"),
-                      BottomNavigationBarItem(
-                          icon: CustomSvg(
-                            svg: AppIcons.chatFilled,
-                            color: NavCubit.changeColor(1, currentIndex),
-                          ),
-                          label: "Chat Bot"),
-                      BottomNavigationBarItem(
-                          icon: CustomSvg(
-                            svg: AppIcons.calender,
-                            color: NavCubit.changeColor(2, currentIndex),
-                          ),
-                          label: "Calender"),
-                      BottomNavigationBarItem(
-                          icon: CustomSvg(
-                            svg: AppIcons.profile,
-                            color: NavCubit.changeColor(3, currentIndex),
-                          ),
-                          label: "Profile"),
-                    ]),
-              ],
-            ),
+            bottomNavigationBar: BottomNavigationBar(
+              elevation: 0,
+                unselectedItemColor: AppColors.grey,
+                selectedItemColor: AppColors.primary,
+                type: BottomNavigationBarType.fixed,
+                onTap: (page) {
+                  cubit.changePage(page);
+                },
+                currentIndex: currentIndex,
+                backgroundColor: Colors.white,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: CustomSvg(
+                          svg: AppIcons.home,
+                          color: NavCubit.changeColor(0, currentIndex)
+                      ),
+                      label: "Home"),
+                  BottomNavigationBarItem(
+                      icon: CustomSvg(
+                        svg: AppIcons.chatFilled,
+                        color: NavCubit.changeColor(1, currentIndex),
+                      ),
+                      label: "Chat Bot"),
+                  BottomNavigationBarItem(
+                      icon: CustomSvg(
+                        svg: AppIcons.calender,
+                        color: NavCubit.changeColor(2, currentIndex),
+                      ),
+                      label: "Calender"),
+                  BottomNavigationBarItem(
+                      icon: CustomSvg(
+                        svg: AppIcons.profile,
+                        color: NavCubit.changeColor(3, currentIndex),
+                      ),
+                      label: "Profile"),
+                ]),
             body: cubit.tabsOfUser[currentIndex]);
       }),
     );
