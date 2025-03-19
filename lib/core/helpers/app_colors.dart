98507197c21536bf82c726c795a6b5e20a003d27
ranamohyy <ranaamr22@gmail.com>
2025-03-19 17:57:06 +0200

    add color of input field

--- lib/core/helpers/app_colors.dart
+++ lib/core/helpers/app_colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors{
   static Color primary=const Color(0xff125B9A);
   static Color grey=const Color(0xff8A8A8A);
+  static Color outlinedInputField=const Color(0xffB2AEAE);
 }
