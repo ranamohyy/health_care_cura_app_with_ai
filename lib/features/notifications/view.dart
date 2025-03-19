b25cd0012e1984aa4e95a2614e407efb00f868eb
ranamohyy <ranaamr22@gmail.com>
2025-03-19 05:25:46 +0200

    finish implement get all notifications

--- lib/features/notifications/view.dart
+++ lib/features/notifications/view.dart
@@ -1,54 +1,62 @@
-import 'package:cura/core/helpers/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import '../controller/notifications_cubit/notifications_cubit.dart';
 import '../widgets/custom_app_bar.dart';
 import '../widgets/health_instriction_card.dart';
 
 class NotificationsView extends StatelessWidget {
   const NotificationsView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(title: "Notifications"),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 16),
-              HealthInstructionCard(
-                showButton: true,
-                icon: true,
-                title: "Additional Check-Up Recommended",
-                description:
-                    "We recommend an additional medical check-up based on your entered symptoms",
-              ),
-              HealthInstructionCard(
-                icon: true,
-                title: "Time for Your Routine Check-Up",
-                description:
-                    "Routine check-ups help maintain your health and detect issues early",
-              ),
-              HealthInstructionCard(
-                icon: true,
-                image: AppIcons.message,
-                title: "Important Reminder",
-                description:
-                    "Don’t forget to review the symptoms you’ve entered for an accurate diagnosis",
-              ),
-              HealthInstructionCard(
-                icon: true,
-                title: "Analysis Complete",
-                description:
-                    "Your data has been successfully analyzed. You can now view the results",
-              ),
-              HealthInstructionCard(
-                icon: true,
-                title: "Your Health Condition Needs Follow-up",
-                description: "It is important to follow up with a specialist",
-              ),
-            ],
+    return BlocProvider(
+      create: (context) => NotificationsCubit()..getAllNotifications(),
+      child: Scaffold(
+        appBar: const MyAppBar(title: "Notifications"),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
+                const HealthInstructionCard(
+                  showButton: true,
+                  icon: true,
+                  title: "Additional Check-Up Recommended",
+                  description:
+                      "We recommend an additional medical check-up based on your entered symptoms",
+                ),
+                BlocBuilder<NotificationsCubit, NotificationsState>(
+                    builder: (context, state) {
+                  if (state is NotificationsLoading) {
+                    return Skeletonizer(
+                        child: Column(
+                      children: List.generate(
+                        4,
+                        (index) => const HealthInstructionCard(
+                          icon: true,
+                          title: 'loading',
+                          description: 'loading',
+                        ),
+                      ),
+                    ));
+                  }
+                  final cubit = context.read<NotificationsCubit>();
+                  return Column(
+                    children: List.generate(
+                      cubit.notifications.length,
+                      (index) => HealthInstructionCard(
+                        icon: true,
+                        title: cubit.notifications[index].title ?? '',
+                        description:
+                            cubit.notifications[index].description ?? '',
+                      ),
+                    ),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
