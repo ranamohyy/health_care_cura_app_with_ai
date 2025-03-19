840adad1aed0873887986a8b73fb6862c1e57c43
ranamohyy <ranaamr22@gmail.com>
2025-03-19 05:24:27 +0200

    finish add states for notification cubit

---
+++ lib/features/controller/notifications_cubit/notifications_state.dart
@@ -0,0 +1,7 @@
+part of 'notifications_cubit.dart';
+
+abstract class NotificationsState {}
+
+ class NotificationsInitial extends NotificationsState {}
+ class NotificationsLoading extends NotificationsState {}
+ class NotificationsSuccess extends NotificationsState {}
