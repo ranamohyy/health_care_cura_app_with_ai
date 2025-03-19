85dc22984b57223ca37ba4265556cd10dd97b9f0
ranamohyy <ranaamr22@gmail.com>
2025-03-19 05:20:08 +0200

    add input decoration theme and edit theme of nav bar

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -1,13 +1,37 @@
 import 'package:cura/core/helpers/app_colors.dart';
 import 'package:flutter/material.dart';
+
+import 'helpers/styles.dart';
  ThemeData get appTheme=>ThemeData(
   scaffoldBackgroundColor: Colors.white,
+  inputDecorationTheme: InputDecorationTheme(
+       hintStyle: kTextStyle14Grey,
+       focusedBorder:  OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.grey.shade900),
+        borderRadius: BorderRadius.circular(40),),
+       border: OutlineInputBorder(
+        borderSide: BorderSide(color: AppColors.grey),
+        borderRadius: BorderRadius.circular(40)
+        ,
+       ),
+
+  ),
   bottomNavigationBarTheme: BottomNavigationBarThemeData(
       selectedItemColor:AppColors.primary,
+      unselectedLabelStyle:const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
+      selectedLabelStyle:const TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
       unselectedItemColor: Colors.grey,
       showSelectedLabels: true,
       elevation: 0,
       backgroundColor: Colors.white,
-      type:BottomNavigationBarType.fixed ),
+
+      type:BottomNavigationBarType.fixed
+  ),
   useMaterial3: true,
 );
