c2f672ee211e188882f03c72803c8e1c2ab7fad0
ranamohyy <ranaamr22@gmail.com>
2025-03-19 05:23:57 +0200

    finish getting all notifications for user

---
+++ lib/features/controller/notifications_cubit/notifications_cubit.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../core/dio_helper.dart';
+import '../../../core/end_points.dart';
+import '../../../models/notifications_model.dart';
+part 'notifications_state.dart';
+
+class NotificationsCubit extends Cubit<NotificationsState> {
+  NotificationsCubit() : super(NotificationsInitial());
+List<NotificationsModel>notifications=[];
+  Future<void> getAllNotifications() async {
+    try {
+      emit(NotificationsLoading());
+      final response = await DioHelper.get(GET_NOTIFICATIONS);
+      if (response.isSuccess) {
+        final List<NotificationsModel> list =List<NotificationsModel>.from(
+            (response.data?['data'] ?? []).map((e) =>
+                NotificationsModel.fromJson(e)));
+        notifications.clear();
+        notifications.addAll(list);
+        emit(NotificationsSuccess());
+      }
+    } catch (e) {
+     log("the error cause ${e.toString()}");
+      emit(NotificationsInitial());
+    }
+  }
+
+}
