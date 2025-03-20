c8e5ad635ef11a580b10ca6bc7935cf72c09ec8c
ranamohyy <ranaamr22@gmail.com>
2025-03-20 22:04:59 +0200

    change end point

--- lib/core/end_points.dart
+++ lib/core/end_points.dart
@@ -1,4 +1,4 @@
-String BASE_URL = 'https://06c3-196-134-167-162.ngrok-free.app/';
+String BASE_URL = 'https://8668-196-134-167-162.ngrok-free.app/';
 //6932-156-197-161-208.ngrok-free.app
 String GET_NOTIFICATIONS='api/Notification/6b552284-f92b-46c5-a5b9-6eaa27bf174b';
 String SEND_MESSAGE="api/Message/send";
