582d1794b38940c7091c82567a5e57e71664d1c8
ranamohyy <ranaamr22@gmail.com>
2025-03-21 01:20:59 +0200

    update base url

--- lib/core/end_points.dart
+++ lib/core/end_points.dart
@@ -1,3 +1,3 @@
-String BASE_URL = 'https://8668-196-134-167-162.ngrok-free.app/';
+String BASE_URL = 'http://localhost:5164/';
 String GET_NOTIFICATIONS='api/Notification/6b552284-f92b-46c5-a5b9-6eaa27bf174b';
 String SEND_MESSAGE="api/Message/send";
